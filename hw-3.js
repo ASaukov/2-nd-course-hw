e19ac1ef19148f3a2585e44d3ceaaa0314a710be
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-18 19:43:52 +0500

    completed task 1

---
+++ hw-3.js
@@ -0,0 +1,8 @@
+        // task 1  //
+let password = 'акула';
+let enterPass = prompt('Введите пароль');
+if (password === enterPass) {
+    console.log('Пароль введен верно');
+} else {
+    console.log('Пароль введен неправильно');
+}
