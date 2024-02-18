ddb971879eeb2c30fe1dad723e7e5df6425fcace
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-18 23:37:09 +0500

    completed task 5

--- hw-3.js
+++ hw-3.js
@@ -20,3 +20,33 @@ console.log(d > 100 || e > 100 ? 'Верно' : 'Неверно');
 let a = '2';
 let b = '3';
 alert(Number(a) + Number(b));
+
+        // task 5 //
+let monthNumber = Number(prompt("Укажите номер месяца"));
+
+switch (monthNumber) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("Зима");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("Весна");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("Лето");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("Осень");
+        break;
+
+    default:
+        console.log("Такого месяца не существует");
+        break;
+}
