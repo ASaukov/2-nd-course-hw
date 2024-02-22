2ed7743d6b3dacd0eeda63d1a900794514aaa089
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-22 18:37:16 +0500

    completed task 4

--- hw-4.js
+++ hw-4.js
@@ -12,3 +12,14 @@ for (let i = 1; i < 6; i++) {
 for (let i = 7; i < 23; i++) {
     console.log(i);
 }
+
+        // 4 //
+const obj = {
+    Коля: '200',
+    Вася: '300',
+    Петя: '400',
+}
+for (let key in obj) {
+    console.log(`${key} - зарплата ${obj[key]} долларов.`);
+}
+
