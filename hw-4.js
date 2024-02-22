a21483df60fc2791ebad9e6eb626a1d3579f73b8
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-22 18:12:14 +0500

    completed task 3

--- hw-4.js
+++ hw-4.js
@@ -4,6 +4,11 @@ for (let index = 0; index < 2; index++) {
 }
 
         // 2 //
-for (let i = 1; i <6; i++) {
+for (let i = 1; i < 6; i++) {
+    console.log(i);
+}
+
+        // 3 //
+for (let i = 7; i < 23; i++) {
     console.log(i);
 }
