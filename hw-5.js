8e82846c3061e41b40cac988f6631882d4ea03c3
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-28 01:51:44 +0500

    completed task 6

--- hw-5.js
+++ hw-5.js
@@ -6,13 +6,13 @@ console.log(smallNumber(6, 4));
 
         // 2 //
 let parity = x => {
-        return x % 2 ===0 ? 'Число четное' : 'Число не четное';
+        return x % 2 === 0 ? 'Число четное' : 'Число не четное';
 }
 console.log(parity(10));
 
         // 3 //
 let squareNum = c => c ** 2;
-console.log(squareNum(8)); 
+console.log(squareNum(8));
 
 let squareNumber = d => d ** 2;
 let result = squareNumber(7);
@@ -37,3 +37,13 @@ let checkingNum = (a, b) => {
 }
 console.log(checkingNum(4, 'пять'));
 
+        // 6 //
+let n = Number(prompt("Введите любое число"));
+let userNum = n => {
+        if (isNaN(n)) {
+                alert('Переданный параметр не является числом');
+        } else {
+                alert(`${n} в кубе равняется ${n ** 3}`);
+        }
+}
+userNum(n);
