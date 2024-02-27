01e9974ce81c86eb3e337903e1349cafeb3dd1f6
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-02-27 19:41:05 +0500

    completed task 2

--- hw-5.js
+++ hw-5.js
@@ -5,3 +5,8 @@ let smallNumber = (a, b) => {
 console.log(smallNumber(6, 4));
 
         // 2 //
+let parity = x => {
+        return x % 2 ===0 ? 'Число четное' : 'Число не четное';
+}
+console.log(parity(10));
+
