29cc645d237a52fd3e0cf34d96a3ca719856bd89
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-03-01 18:22:28 +0500

    completed task 8

--- hw-5.js
+++ hw-5.js
@@ -72,3 +72,20 @@ console.log(circle1.area());
 console.log(circle1.perimeter());
 console.log(circle2.area());
 console.log(circle2.perimeter());
+
+        // 8 //
+function season() {
+        let monthNumber = Number(prompt("Введите номер месяца"));
+        if (monthNumber === 12 || monthNumber === 1 || monthNumber === 2) {
+                alert("Зима");
+        } else if (monthNumber === 3 || monthNumber === 4 || monthNumber === 5) {
+                alert("Весна");
+        } else if (monthNumber === 6 || monthNumber === 7 || monthNumber === 8) {
+                alert("Лето");
+        } else if (monthNumber === 9 || monthNumber === 10 || monthNumber === 11) {
+                alert("Осень");
+        } else {
+                alert("Вы ввели недопустимое значение");
+                season();
+        }
+}
