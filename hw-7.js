7e74a6122cb31177bdf23178b8d29e985128c291
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-03-14 16:49:40 +0500

    completed task 2

--- hw-7.js
+++ hw-7.js
@@ -2,3 +2,13 @@
 const str1 = 'js';
 console.log(str1.toUpperCase());
 
+    // 2 //
+const arr2 = ['Сок Томатный', "Томатный соус", "Яблочное пюре", "Сок яблочный", "сок апельсиновый"];
+const search = 'сок';
+arr2.forEach((el) => {
+    if (el.toLowerCase().startsWith(search.toLowerCase())) {
+        console.log(el);
+    }
+});
+
+
