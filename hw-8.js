0bfc71fada8082d619f558ce52e0c8f83bf4bfb1
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-03-19 02:35:08 +0500

    completed task 2

--- hw-8.js
+++ hw-8.js
@@ -16,3 +16,31 @@ console.log(people.sort((a, b) => {
         return 0
     }
 })) ;
+
+    // 2 //
+    function isPositive(type) {
+            return type > 0;
+        }
+        function isMale(man) {
+            return man.gender === 'male';
+        }
+        function filter(arr, callback) {
+        const arr2 = [];
+            for (let i = 0; i < arr.length; i++) {
+            if (callback(arr[i])) {
+            arr2.push(arr[i]);
+            }
+        }
+         return arr2;
+    }
+        
+        console.log(filter([3, -4, 1, 9], isPositive));
+        
+        const people2 = [
+           {name: 'Глеб', gender: 'male'},
+           {name: 'Анна', gender: 'female'},
+           {name: 'Олег', gender: 'male'},
+           {name: 'Оксана', gender: 'female'}
+        ];
+        
+        console.log(filter(people2, isMale))
